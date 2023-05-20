d819423ead1aea5e634ac6d2207d6df3280096f6
Boria138 <boriabloger@protonmail.com>
2023-05-20 18:06:49 +0600

    Fixed a few minor bugs

--- services/usr/bin/dirty_ratio.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-available_mem=$(free -g | grep Mem | awk '{print $7}')
-if [ $available_mem -le 5 ]; then
-    sysctl -w vm.dirty_ratio=10
-elif [ $available_mem -le 13 ]; then
-    sysctl -w vm.dirty_ratio=4
-fi
