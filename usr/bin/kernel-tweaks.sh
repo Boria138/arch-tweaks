daeebab266b43d437fe1d406287fb5523edb5b86
Boria138 <boriabloger@protonmail.com>
2023-05-26 13:27:44 +0600

    Fix dirty ratio

--- usr/bin/kernel-tweaks.sh
+++ usr/bin/kernel-tweaks.sh
@@ -3,20 +3,20 @@
 # Get the available memory in gigabytes
 available_mem=$(free -g | awk '/Mem/ {print $7}')
 
-# Calculate the dirty_ratio based on available memory (set to 50% of available memory)
-dirty_ratio=$((available_mem / 2))
-
-# Calculate the background_ratio based on available memory (set to 25% of available memory)
-background_ratio=$((available_mem / 4))
-
-# Update the dirty_ratio and background_ratio values
-sysctl -w vm.dirty_ratio="$dirty_ratio"
-sysctl -w vm.dirty_background_ratio="$background_ratio"
+if [ $available_mem -le 5 ]; then
+    sysctl -w vm.dirty_ratio=10
+    sysctl -w vm.dirty_background_ratio=5
+elif [ $available_mem -le 13 ]; then
+    sysctl -w vm.dirty_ratio=4
+    sysctl -w vm.dirty_background_ratio=2
+fi
 
 # Check if a swap file or partition exists
 if [[ $(swapon --show) ]]; then
+  # A swap file or partition exists, set vm.swappiness to 100
   sysctl -w vm.swappiness=100
 fi
+
 # Iterate over the block devices and check if they are zram devices
 for device in /sys/block/zram*; do
     if [[ -d "$device" ]]; then
