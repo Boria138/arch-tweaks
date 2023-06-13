21bf1b29f637375bf9435a9afcd6d7e98c47f5b6
Boria138 <boriabloger@protonmail.com>
2023-06-13 13:02:11 +0600

    Update profile.d and kernel tweaks
    Add GNOME-xorg cursor fix
    Add vm.vfs_cache_pressure auto set

--- etc/profile.d/general.sh
+++ etc/profile.d/general.sh
@@ -4,9 +4,26 @@ export PATH=~/.local/bin:$PATH
 # Fix pip install error if tmp dir is full
 export TMPDIR='/var/tmp'
 
-export EDITOR=micro
+# Set $EDITOR to micro if is empty
+if [ -z "$EDITOR"]; then
+  export EDITOR=micro 
+fi
 
 # Allow npm to run not from sudo (For security)
 if pacman -Qi npm &> /dev/null; then
   npm config set prefix ~/.local
+fi
+
+# Set kvantum QT engine in GTK DE
+if [[ $DESKTOP_SESSION != "plasma" ]]; then
+  if [ -z "$QT_QPA_PLATFORMTHEME" ]; then
+    if pacman -Qi kvantum &> /dev/null; then
+      export QT_STYLE_OVERRIDE=kvantum
+    fi
+  fi
+fi
+
+# Fix cursor bug in GNOME XORG
+if [[ $DESKTOP_SESSION == "gnome-xorg" ]]; then
+  xset -b off
 fi
